9b43c45f93cf70c7f5841141c418a95b6a6938c2
Bart Massey <bart@cs.pdx.edu>
2016-07-20 00:00:32 -0700

    fixed address copy-paste bug that was confusing debugging mightily

--- cy.c
+++ cy.c
@@ -6,6 +6,6 @@ int main(void) {
     char *s1 = myfun();
     printf("%u: %s\n", (unsigned long) s1, s1);
     char *s2 = myfun();
-    printf("%u: %s\n", (unsigned long) s1, s2);
+    printf("%u: %s\n", (unsigned long) s2, s2);
     return 0;
 }
